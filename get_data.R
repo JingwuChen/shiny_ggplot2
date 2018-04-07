d672ce817a7ef4871ef4906b0affb7e3318f3835
JingwuChen <sohoyouwo0301@163.com>
2018-04-08 03:35:51 +0800

    从安居客上爬虫获取数据

---
+++ get_data.R
@@ -0,0 +1,42 @@
+library(rvest)
+#library(RSelenium)
+library(stringr)
+
+get_response<-function(url){
+  #pJS <- phantom()
+  #remDr <- remoteDriver(browserName = "phantomjs")
+  #remDr$open() 
+  #remDr$navigate(url)
+  #response<- remDr$getPageSource()[[1]]
+  #content<-read_html(response,encoding = "utf-8")
+  content<-read_html(url,encoding = "utf-8")
+  price<-html_nodes(content,css = "#list-content > div > div.zu-side > p > strong") %>% html_text()
+  price<-as.numeric(price)
+  area<-html_nodes(content,css = "#list-content > div > div.zu-info > address") %>% html_text()
+  area<-trimws(area)
+  district<-substr( trimws(substring(area,20)),1,2)
+  size<-html_nodes(content,css = "#list-content > div > div.zu-info > p.details-item.tag") %>% html_text()
+  size<-trimws(size)
+  house<-strsplit(size,split = "|",fixed = T)
+  house_layout<-sapply(house,"[",1)
+  house_size<-sapply(house,"[",2)
+  house_size<-as.numeric(str_extract(house_size, "\\d+"))
+  data<-data.frame(district,house_layout,house_size,price)
+  #remDr$close
+  #pJS$stop()
+  return(data)
+}
+get_data<-function(n=10) {
+  data<-c()
+  if(n<=50){
+  for(i in 1:n){
+    url<-sprintf("https://gz.zu.anjuke.com/fangyuan/p%d/",i)
+    master<-get_response(url)
+    data<-rbind(master,data)
+   
+  }
+    
+  }
+  data<-data[data$district!="广州",]
+  return(data)
+}
